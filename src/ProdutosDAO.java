4c6aa6b1370ffefd55358d62a88ef8a6022abede
cleonirs <cleonirs@gmail.com>
2025-04-06 05:23:43 -0300

    Commit #4 Funcionalidades btnCadastrar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,50 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
-import java.sql.PreparedStatement;
-import java.sql.Connection;
-import javax.swing.JOptionPane;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-
-public class ProdutosDAO {
-    
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto) throws SQLException{
-    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
-
-   
-        conn = new conectaDAO().connectDB();
-        prep = conn.prepareStatement(sql);
-        prep.setString(1, produto.getNome());
-        prep.setInt(2, produto.getValor());
-        prep.setString(3, produto.getStatus());
-
-        prep.executeUpdate();
-        
-        
-    }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
-
+/* * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template *//** * * @author Adm */import java.sql.PreparedStatement;import java.sql.Connection;import javax.swing.JOptionPane;import java.sql.ResultSet;import java.sql.SQLException;import java.util.ArrayList;public class ProdutosDAO {        Connection conn;    PreparedStatement prep;    ResultSet resultset;    ArrayList<ProdutosDTO> listagem = new ArrayList<>();        public void cadastrarProduto (ProdutosDTO produto) throws SQLException{    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";           conn = new conectaDAO().connectDB();        prep = conn.prepareStatement(sql);        prep.setString(1, produto.getNome());        prep.setInt(2, produto.getValor());        prep.setString(3, produto.getStatus());        prep.executeUpdate();                    }        public ArrayList<ProdutosDTO> listarProdutos(){                return listagem;    }                    }
